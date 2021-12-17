a11099f9c6673159f3b72c4c0ab28f1798549c2b
liangzimiao <2968274386@qq.com>
2021-12-17 19:35:24 +0900

    更合理的加载guess插件

--- src/plugins/pcrgames/__init__.py
+++ src/plugins/pcrgames/__init__.py
@@ -107,3 +107,7 @@ _sub_plugins = set()
 _sub_plugins |= nonebot.load_plugins(
     str((Path(__file__).parent / "plugins").
     resolve()))
+_sub_plugins = set()
+_sub_plugins |= nonebot.load_plugins(
+    str((Path(__file__).parent / "plugins/guess").
+    resolve()))
