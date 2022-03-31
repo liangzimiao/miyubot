47d1645fafdfd45edfa902ab9b7d21ee15c3f43b
Erisu <1269732086@qq.com>
2022-03-31 10:48:15 +0800

    :art: 删除send_guild_message的config

--- utils/__init__.py
+++ utils/__init__.py
@@ -12,42 +12,27 @@ import time
 from collections import defaultdict
 from datetime import datetime, timedelta
 from io import BytesIO
-from nonebot.adapters.onebot.v11.event import MessageEvent,GroupMessageEvent
-from nonebot_plugin_guild_patch import GuildMessageEvent
+
 import pytz
 import unicodedata
 import zhconv
 from PIL import Image
 from nonebot import get_driver, logger
-from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11.event import MessageEvent
 from nonebot.adapters.onebot.v11.event import Sender
 from nonebot_plugin_guild_patch import GuildMessageEvent, patched_send
-from pydantic import BaseSettings
 
-
-class Config(BaseSettings):
-    bot_id: str
-    bot_guild_id: str
-
-    class Config:
-        extra = "ignore"
-
-
-global_config = get_driver().config
-config = Config(**global_config.dict())
-
-bot_id = config.dict().get("bot_id")
-bot_guild_id = config.dict().get("bot_guild_id")
+from utils.base_config import BotInfo
 
 
 async def send_guild_message(guild_id, channel_id, message_str):
-    bot = get_driver().bots[str(bot_id)]
-    sender = Sender(user_id=bot_id)
-    event = GuildMessageEvent(time=int(time.time()), self_id=bot_id,
-                              post_type='message', sub_type='channel', user_id=bot_guild_id,
+    bot = get_driver().bots[str(BotInfo.bot_id)]
+    sender = Sender(user_id=BotInfo.bot_id)
+    event = GuildMessageEvent(time=int(time.time()), self_id=BotInfo.bot_id,
+                              post_type='message', sub_type='channel', user_id=BotInfo.bot_guild_id,
                               message_type='guild', message_id='557-1040527614', guild_id=guild_id,
                               channel_id=channel_id, message="0",
-                              sender=sender, self_tiny_id=int(bot_guild_id))
+                              sender=sender, self_tiny_id=int(BotInfo.bot_guild_id))
     await patched_send(bot, event, message_str)
 
 
